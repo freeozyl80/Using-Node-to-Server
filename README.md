d7f1a7a3547277550a2e55300b8fbb0c6b72f44e
zhangyunlu <zhangyunlu@baidu.com>
2017-03-22 21:02:37 +0800

    zhangyunlu

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Using-Node-to-Server
 Just learn something fun of node
+
+### node 几个重要的模块 os, path, domain
---
+++ test/main.js
@@ -0,0 +1,66 @@
+// 引入 events 模块
+var events = require('events');
+// 创建 eventEmitter 对象
+var eventEmitter = new events.EventEmitter();
+
+// 创建事件处理程序
+var connectHandler = function connected() {
+   console.log('连接成功。');
+  
+   // 触发 data_received 事件 
+   eventEmitter.emit('data_received');
+}
+
+// 绑定 connection 事件处理程序
+eventEmitter.on('connection', connectHandler);
+ 
+// 使用匿名函数绑定 data_received 事件
+eventEmitter.on('data_received', function(){
+   console.log('数据接收成功。');
+});
+
+// 触发 connection 事件 
+eventEmitter.emit('connection');
+
+console.log("程序执行完毕。");
+
+
+/* ----------------------------------------------------- */
+process.on('exit', function(code) {
+
+  // 以下代码永远不会执行
+  setTimeout(function() {
+    console.log("该代码不会执行");
+  }, 0);
+  
+  console.log('退出码为:', code);
+});
+console.log("程序执行结束");
+
+
+/* -------------------------------------------------------- */
+
+// 输出到终端
+process.stdout.write("Hello World!" + "\n");
+
+// 通过参数读取
+process.argv.forEach(function(val, index, array) {
+   console.log(index + ': ' + val);
+});
+
+// 获取执行路局
+console.log(process.execPath);
+
+
+// 平台信息
+console.log(process.platform);
+
+// $ node main.js
+// Hello World!
+// 0: node
+// 1: /web/www/node/main.js
+// /usr/local/node/0.10.36/bin/node
+// darwin
+
+
+/* -------------------------------------------------------- */
